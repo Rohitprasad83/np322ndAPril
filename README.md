88e72e9fc87956502132d54b1b29ea81b88b768a
Rohitprasad83 <pdrohit674@gmail.com>
2022-04-22 23:59:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-avdzes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-avdzes)
---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const button = document.querySelector('button');
+const table = document.querySelectorAll('tbody');
+
+button.addEventListener('click', () => {
+  const rowNum = 'Row ' + prompt('Enter the row number');
+  const colNum = 'Cell ' + prompt('Enter the col number');
+  const newContent = prompt('Enter the new content');
+
+  const place = rowNum + ' ' + colNum;
+  table[0].innerHTML = table[0].innerHTML.replace(place, newContent);
+});
